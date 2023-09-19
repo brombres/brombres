fb245393e9debc93652c815047a6c834dd41d5c9
Brom Bresenham <brom.bresenham@runegate.com>
2023-09-19 17:58:04 -0400

    [WASM Attempt 1] FAIL

---
+++ src/routes/warlock/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Emscripten from '$lib/svelte-emscripten';
+  import {default as create_module} from './Test.js';
+</script>
+
+<svelte:head>
+	<title>Warlock</title>
+	<meta name="description" content="Warlock Web App" />
+</svelte:head>
+
+<Emscripten
+  module={create_module}
+  canvas={true}
+  console={true}
+  verticalOrientation={false}
+  options={
+    {
+      autorun: true,
+      wasmPath: './Test.wasm'
+    }
+  }
+/>
